09abf82be9eaf81b5a143bc113f1c40a03066999
Vincent Avez <vincent.avez22@gmail.com>
2023-04-27 23:59:17 +0200

    added responsive on homepage

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer(){
             <footer className="flex flex-col">
                 <section className="flex flex-col bg-gray-950 text-white py-10">
                     <h1 className="text-center p-8 font-karla text-3xl">Nos engagements envers nos clients</h1>
-                    <div className="flex border-t w-5/6 mx-auto border-white">
-                        <div className="p-5">
+                    <div className="grid grid-cols-6 border-t w-5/6 mx-auto border-white">
+                        <div className="p-5 col-span-6 sm:col-span-2">
                             <svg 
                             className="mx-auto"
                             width="50" 
@@ -32,7 +32,7 @@ export default function Footer(){
                                 libero consectetur architecto maxime hic eius autem.
                             </p>
                         </div>
-                        <div className="p-5">
+                        <div className="p-5 col-span-6 sm:col-span-2">
                             <svg
                             className="mx-auto"
                             width="50" 
@@ -55,7 +55,7 @@ export default function Footer(){
                                 libero consectetur architecto maxime hic eius autem.
                             </p>
                         </div>
-                        <div className="p-5">
+                        <div className="p-5 col-span-6 sm:col-span-2">
                             <svg 
                             className="mx-auto" 
                             width="50" 
@@ -76,11 +76,55 @@ export default function Footer(){
                     </div>
                 </section>
                 <div className="bg-gradient-to-b from-gray-950 via-gray-950 to-black h-2"></div>
-                <section className="flex flex-col bg-black h-60 text-white">
-                    <div className="flex justify-center space-x-24 p-10 font-karla text-2xl">
-                        <h1>A propos</h1>
-                        <h1>SpaceZone</h1>
-                        <div className="flex flex-col space-y-5">
+                <section className="flex flex-col items-center bg-black text-white h-full">
+                    <div className="grid grid-cols-6 gap-5 justify-center items-center p-10 font-karla text-2xl">
+                        <div className="flex flex-col col-span-6 lg:col-span-2">
+                            <h1 className="text-center">
+                                Réseau Sociaux
+                            </h1>
+                            <div className="flex items-center justify-center space-x-5">
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M16 4H8a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V8a4 4 0 0 0-4-4Z"></path>
+                                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"></path>
+                                    <path d="M16.5 7.5v.001"></path>
+                                </svg>
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M22 4.009c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.322 12 7.999v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497-3.753c-.002-.249 1.51-2.772 1.818-4.013v.001Z"></path>
+                                </svg>
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z"></path>
+                                    <path d="M9 19c-4.3 1.4-4.3-2.5-6-3"></path>
+                                </svg>
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17 5H7a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h10a4 4 0 0 0 4-4V9a4 4 0 0 0-4-4Z"></path>
+                                    <path d="m10 9 5 3-5 3V9Z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div className="flex flex-col col-span-6 lg:col-span-2">
+                            <h1>
+                                En collaboration avec
+                            </h1>
+                            <div className="flex space-x-5">
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M16 4H8a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V8a4 4 0 0 0-4-4Z"></path>
+                                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"></path>
+                                    <path d="M16.5 7.5v.001"></path>
+                                </svg>
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M22 4.009c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.322 12 7.999v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497-3.753c-.002-.249 1.51-2.772 1.818-4.013v.001Z"></path>
+                                </svg>
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15 21v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21h6Z"></path>
+                                    <path d="M9 19c-4.3 1.4-4.3-2.5-6-3"></path>
+                                </svg>
+                                <svg className="hover:scale-110" width="30" height="30" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17 5H7a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h10a4 4 0 0 0 4-4V9a4 4 0 0 0-4-4Z"></path>
+                                    <path d="m10 9 5 3-5 3V9Z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div className="flex flex-col col-span-6 lg:col-span-2">
                             <h1>
                                 Réseau Sociaux
                             </h1>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,16 @@ export default function Header(){
         <>
             <div className="background-bar shape"></div>
             <header className="header">
-                <h1 className="title">SpaceZone, spécialisé dans la vente en ligne</h1>
-                <div className="best-seller">
-                    <h2 className="title-best-seller">Meilleures ventes : Découvrez les produits les plus populaires parmi notre communauté de clients satisfaits.</h2>
-                </div>
-                <div className="arrow-box">
-                    <svg className="arrow" width="50" height="50" fill="black" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 4v8h3.586a1 1 0 0 1 .707 1.707l-6.586 6.586a1 1 0 0 1-1.414 0l-6.586-6.586A1 1 0 0 1 5.414 12H9V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v0Z"></path>
-                    </svg>
+                <div className="body-header">
+                    <h1 className="title">SpaceZone, spécialisé dans la vente en ligne</h1>
+                    <div className="best-seller">
+                        <h2 className="title-best-seller">Meilleures ventes : Découvrez les produits les plus populaires parmi notre communauté de clients satisfaits.</h2>
+                    </div>
+                    <div className="arrow-box">
+                        <svg className="arrow" width="50" height="50" fill="black" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 4v8h3.586a1 1 0 0 1 .707 1.707l-6.586 6.586a1 1 0 0 1-1.414 0l-6.586-6.586A1 1 0 0 1 5.414 12H9V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v0Z"></path>
+                        </svg>
+                    </div>
                 </div>
             </header>
         </>
